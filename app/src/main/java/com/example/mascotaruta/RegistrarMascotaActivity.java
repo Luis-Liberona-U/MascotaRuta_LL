15066d1c42927846cd6d7d58151dba25e9ac006c
Benjamin <b.molina19@alumnos.santotomas.cl>
2026-09-11 12:54:06 -0300

    feat: Implementacion de formulario de registro de mascota.

--- app/src/main/java/com/example/mascotaruta/RegistrarMascotaActivity.java
+++ app/src/main/java/com/example/mascotaruta/RegistrarMascotaActivity.java
@@ -1,6 +1,15 @@
 package com.example.mascotaruta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +19,111 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrarMascotaActivity extends AppCompatActivity {
 
+    private Button btnRegistrar;
+    private RadioGroup rgCategoria;
+    private CheckBox cbEsterilizacion;
+    private CheckBox cbSeguimiento;
+
+    private EditText edtNombrePersona, edtNombreMascota;
+
+    public static final String EXTRA_NOMBREPERSONA = "NOMBREPERSONA";
+    public static final String EXTRA_NOMBREMASCOTA = "NOMBREMASCOTA";
+    public static final String EXTRA_CATEGORIA = "CATEGORIA";
+    public static final String EXTRA_COMPROMISOS = "COMPROMISOS";
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registrar_mascota);
+
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+        rgCategoria = findViewById(R.id.rgCategoria);
+        cbEsterilizacion = findViewById(R.id.cbEsterilizacion);
+        cbSeguimiento = findViewById(R.id.cbSeguimiento);
+        edtNombrePersona = findViewById(R.id.edtNombrePersona);
+        edtNombreMascota = findViewById(R.id.edtNombreMascota);
+
+
+
+        btnRegistrar.setOnClickListener(v ->{
+
+            String nombrePersona = edtNombrePersona.getText().toString().trim();
+            String nombreMascota = edtNombreMascota.getText().toString().trim();
+
+            if (nombrePersona.isEmpty()) {
+                edtNombrePersona.setError("Debes ingresar tu nombre");
+                edtNombrePersona.requestFocus();
+                return;
+            }
+
+            if (nombreMascota.isEmpty()) {
+                edtNombreMascota.setError("Debes ingresar el nombre del animal");
+                edtNombreMascota.requestFocus();
+                return;
+            }
+
+            int idCategoria = rgCategoria.getCheckedRadioButtonId();
+
+            if (idCategoria == -1) {
+                Toast.makeText(RegistrarMascotaActivity.this,"Debes elegir una categoria",Toast.LENGTH_SHORT).show();
+                return;
+
+            }
+
+            RadioButton btnSelecionado = rgCategoria.findViewById(idCategoria);
+
+            String categoria = btnSelecionado.getText().toString();
+
+            String compromisos = "";
+
+
+
+            if (cbEsterilizacion.isChecked()) {
+                compromisos += " Esterilizacion";
+            }
+            if (cbSeguimiento.isChecked()) {
+
+                compromisos += "Seguimiento";
+            }
+
+            if (compromisos.isEmpty()) {
+                Toast.makeText(RegistrarMascotaActivity.this,"Debes selecionar al menos un compromiso", Toast.LENGTH_SHORT).show();
+                return;
+
+            }
+
+            Intent intent = new Intent(
+                    RegistrarMascotaActivity.this,
+                    ConfirmacionActivity.class
+            );
+
+            intent.putExtra(EXTRA_NOMBREPERSONA, nombrePersona);
+            intent.putExtra(EXTRA_NOMBREMASCOTA, nombreMascota);
+            intent.putExtra(EXTRA_CATEGORIA, categoria);
+            intent.putExtra(EXTRA_COMPROMISOS, compromisos);
+
+            startActivity(intent);
+
+
+
+
+
+
+
+
+        });
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
